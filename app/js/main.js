96987e9da779d3702db8b9ba51eacf6a566a7421
Ivan <dykyiivan@gmail.com>
2020-04-08 20:44:30 +0200

    partner section

---
+++ app/js/main.js
@@ -0,0 +1,36 @@
+$(function(){
+
+  //Filter Tasbs
+  let filter = $("[data-filter]");
+
+  filter.on("click", function(event) {
+    event.preventDefault();
+
+    let cat = $(this).data('filter');
+
+    if(cat === "all") {
+      $("[data-category]").removeClass("hide");
+    } else {
+      $("[data-category]").each(function () {
+        let workCat = $(this).data("category");
+
+        if (workCat != cat) {
+          $(this).addClass('hide');
+        } else (
+          $(this).removeClass('hide')
+        )
+
+      });
+    }
+  });
+
+  //Slick Slider
+  $('.partners__slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/partners/right.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/partners/left.png"></button>',
+  });
+
+});
